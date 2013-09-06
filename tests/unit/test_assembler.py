9ec654f29dfbd117d8973e83039747b0f94c9a84
fakeezz <fakeezz@gmail.com>
2013-09-06 19:21:20 -0300

    wip in fix tests

--- tests/unit/test_assembler.py
+++ tests/unit/test_assembler.py
@@ -1,22 +1,26 @@
 import unittest
+import struct
 from chasm import assembler, lexical, syntactic, semantic
 
 
 class AssemblerTestCase(unittest.TestCase):
 
+    def pack(self, opcode):
+        return struct.pack('>H', int('0x%s' % (opcode,), 16))
+
     def test_convert_SYS_node_to_opcode(self):
 
         # Arrange:
-        code = "SYS #fff"
+        code = "SYS 0xfff"
         tokens = lexical.tokenize(code)
         ast = syntactic.Ast(tokens)
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('0FFF', opcodes[0])
+        self.assertEqual(self.pack('0FFF'), opcodes[0])
 
     def test_convert_CLS_node_to_opcode(self):
 
@@ -27,10 +31,10 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('00E0', opcodes[0])
+        self.assertEqual(self.pack('00E0'), opcodes[0])
 
     def test_convert_RET_node_to_opcode(self):
 
@@ -41,66 +45,66 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('00EE', opcodes[0])
+        self.assertEqual(self.pack('00EE'), opcodes[0])
 
     def test_convert_JMP_1NNN_node_to_opcode(self):
 
         # Arrange:
-        code = "JMP #FFF"
+        code = "JMP 0xFFF"
         tokens = lexical.tokenize(code)
         ast = syntactic.Ast(tokens)
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('1FFF', opcodes[0])
+        self.assertEqual(self.pack('1FFF'), opcodes[0])
 
     def test_convert_JMP_BNNN_node_to_opcode(self):
 
         # Arrange:
-        code = "JMP #FFF, V0"
+        code = "JMP 0xFFF, V0"
         tokens = lexical.tokenize(code)
         ast = syntactic.Ast(tokens)
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('BFFF', opcodes[0])
+        self.assertEqual(self.pack('BFFF'), opcodes[0])
 
     def test_convert_CALL_node_to_opcode(self):
 
         # Arrange:
-        code = "CALL #FFF"
+        code = "CALL 0xFFF"
         tokens = lexical.tokenize(code)
         ast = syntactic.Ast(tokens)
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('2FFF', opcodes[0])
+        self.assertEqual(self.pack('2FFF'), opcodes[0])
 
     def test_convert_SE_3XNN_node_to_opcode(self):
 
         # Arrange:
-        code = "SE V0, #FF"
+        code = "SE V0, 0xFF"
         tokens = lexical.tokenize(code)
         ast = syntactic.Ast(tokens)
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('30FF', opcodes[0])
+        self.assertEqual(self.pack('30FF'), opcodes[0])
 
     def test_convert_SE_5XY0_node_to_opcode(self):
 
@@ -111,24 +115,24 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('5010', opcodes[0])
+        self.assertEqual(self.pack('5010'), opcodes[0])
 
     def test_convert_SNE_4XNN_node_to_opcode(self):
 
         # Arrange:
-        code = "SNE V0, #ae"
+        code = "SNE V0, 0xae"
         tokens = lexical.tokenize(code)
         ast = syntactic.Ast(tokens)
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('40AE', opcodes[0])
+        self.assertEqual(self.pack('40AE'), opcodes[0])
 
     def test_convert_SNE_9XY0_node_to_opcode(self):
 
@@ -139,24 +143,24 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('9150', opcodes[0])
+        self.assertEqual(self.pack('9150'), opcodes[0])
 
     def test_convert_ADD_7XNN_node_to_opcode(self):
 
         # Arrange:
-        code = "ADD V2, #FF"
+        code = "ADD V2, 0xFF"
         tokens = lexical.tokenize(code)
         ast = syntactic.Ast(tokens)
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('72FF', opcodes[0])
+        self.assertEqual(self.pack('72FF'), opcodes[0])
 
     def test_convert_ADD_8XY4_node_to_opcode(self):
 
@@ -167,10 +171,10 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('8264', opcodes[0])
+        self.assertEqual(self.pack('8264'), opcodes[0])
 
     def test_convert_OR_node_to_opcode(self):
 
@@ -181,10 +185,10 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('8261', opcodes[0])
+        self.assertEqual(self.pack('8261'), opcodes[0])
 
     def test_convert_AND_node_to_opcode(self):
 
@@ -195,10 +199,10 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('8262', opcodes[0])
+        self.assertEqual(self.pack('8262'), opcodes[0])
 
     def test_convert_XOR_node_to_opcode(self):
 
@@ -209,10 +213,10 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('8263', opcodes[0])
+        self.assertEqual(self.pack('8263'), opcodes[0])
 
     def test_convert_SUB_node_to_opcode(self):
 
@@ -223,10 +227,10 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('8345', opcodes[0])
+        self.assertEqual(self.pack('8345'), opcodes[0])
 
     def test_convert_SHR_node_to_opcode(self):
 
@@ -237,24 +241,24 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('8346', opcodes[0])
+        self.assertEqual(self.pack('8346'), opcodes[0])
 
-    def test_convert_SUBC_node_to_opcode(self):
+    def test_convert_SUBN_node_to_opcode(self):
 
         # Arrange:
-        code = "SUBC V3, V4"
+        code = "SUBN V3, V4"
         tokens = lexical.tokenize(code)
         ast = syntactic.Ast(tokens)
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('8347', opcodes[0])
+        self.assertEqual(self.pack('8347'), opcodes[0])
 
     def test_convert_SHL_node_to_opcode(self):
 
@@ -265,52 +269,52 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('834E', opcodes[0])
+        self.assertEqual(self.pack('834E'), opcodes[0])
 
     def test_convert_LDI_node_to_opcode(self):
 
         # Arrange:
-        code = "LDI #ffe"
+        code = "LDI 0xffe"
         tokens = lexical.tokenize(code)
         ast = syntactic.Ast(tokens)
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('AFFE', opcodes[0])
+        self.assertEqual(self.pack('AFFE'), opcodes[0])
 
     def test_convert_RND_node_to_opcode(self):
 
         # Arrange:
-        code = "RND V1, #FF"
+        code = "RND V1, 0xFF"
         tokens = lexical.tokenize(code)
         ast = syntactic.Ast(tokens)
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('C1FF', opcodes[0])
+        self.assertEqual(self.pack('C1FF'), opcodes[0])
 
     def test_convert_DRW_node_to_opcode(self):
 
         # Arrange:
-        code = "DRW V1, V9, #F"
+        code = "DRW V1, V9, 0xF"
         tokens = lexical.tokenize(code)
         ast = syntactic.Ast(tokens)
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('D19F', opcodes[0])
+        self.assertEqual(self.pack('D19F'), opcodes[0])
 
     def test_convert_SKP_node_to_opcode(self):
 
@@ -321,10 +325,10 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('E09E', opcodes[0])
+        self.assertEqual(self.pack('E09E'), opcodes[0])
 
     def test_convert_SKNP_node_to_opcode(self):
 
@@ -335,10 +339,10 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('E0A1', opcodes[0])
+        self.assertEqual(self.pack('E0A1'), opcodes[0])
 
     def test_convert_STR_node_to_opcode(self):
 
@@ -349,10 +353,10 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('F155', opcodes[0])
+        self.assertEqual(self.pack('F155'), opcodes[0])
 
     def test_convert_FILL_node_to_opcode(self):
 
@@ -363,24 +367,24 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('F165', opcodes[0])
+        self.assertEqual(self.pack('F165'), opcodes[0])
 
     def test_convert_LD_6XNN_node_to_opcode(self):
 
         # Arrange:
-        code = "LD V0, #FF"
+        code = "LD V0, 0xFF"
         tokens = lexical.tokenize(code)
         ast = syntactic.Ast(tokens)
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('60FF', opcodes[0])
+        self.assertEqual(self.pack('60FF'), opcodes[0])
 
     def test_convert_LD_8XY0_node_to_opcode(self):
 
@@ -391,10 +395,10 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('8020', opcodes[0])
+        self.assertEqual(self.pack('8020'), opcodes[0])
 
     def test_convert_LD_FX07_node_to_opcode(self):
 
@@ -405,10 +409,10 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('F107', opcodes[0])
+        self.assertEqual(self.pack('F107'), opcodes[0])
 
     def test_convert_LD_FX0A_node_to_opcode(self):
 
@@ -419,10 +423,10 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('F20A', opcodes[0])
+        self.assertEqual(self.pack('F20A'), opcodes[0])
 
     def test_convert_LD_FX15_node_to_opcode(self):
 
@@ -433,10 +437,10 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('F015', opcodes[0])
+        self.assertEqual(self.pack('F015'), opcodes[0])
 
     def test_convert_LD_FX18_node_to_opcode(self):
 
@@ -447,10 +451,10 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('F318', opcodes[0])
+        self.assertEqual(self.pack('F318'), opcodes[0])
 
     def test_convert_LD_FX1E_node_to_opcode(self):
 
@@ -461,10 +465,10 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('F41E', opcodes[0])
+        self.assertEqual(self.pack('F41E'), opcodes[0])
 
     def test_convert_LD_FX29_node_to_opcode(self):
 
@@ -475,10 +479,10 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('F529', opcodes[0])
+        self.assertEqual(self.pack('F529'), opcodes[0])
 
     def test_convert_LD_FX33_node_to_opcode(self):
 
@@ -489,7 +493,7 @@ class AssemblerTestCase(unittest.TestCase):
         semantic.analyze(ast)
 
         # Act:
-        opcodes = assembler.compile(ast)
+        opcodes = assembler.generate(ast)
 
         # Arrange:
-        self.assertEqual('F633', opcodes[0])
+        self.assertEqual(self.pack('F633'), opcodes[0])
